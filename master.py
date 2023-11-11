09e6bea95da411a2a39e530ee1acc2d05c6a2333
Baelrin <Baelrin-Revel@yandex.ru>
2023-11-11 20:55:57 +0300

    Improved security, comments and added some error handling

--- master.py
+++ master.py
@@ -1,21 +1,27 @@
-import os
+import asyncio
+import datetime
 import json
 import logging
-from dotenv import load_dotenv
+
 import discord
-from discord.ext import commands
-import datetime
 import pytz
+from discord.ext import commands
 
-# Загрузка переменных окружения из .env файла
-load_dotenv()
 
-# Чтение конфигурационного файла
-with open("config.json") as f:
-    config = json.load(f)
+# Read the configuration file
+def read_config():
+    try:
+        with open("config.json") as f:
+            return json.load(f)
+    except FileNotFoundError as e:
+        logging.error(f"Failed to read configuration file: {e}")
+        raise
 
-# Переменные
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+config = read_config()
+
+# Variables
+TOKEN = "TOKEN"
 ADMIN_ROLE_ID = config["ADMIN_ROLE_ID"]
 OLD_ROLE_ID = config["OLD_ROLE_ID"]
 NEW_ROLE_ID = config["NEW_ROLE_ID"]
@@ -25,43 +31,47 @@ TIMEZONE = config["TIMEZONE"]
 PREFIX = config["PREFIX"]
 LOG_FILE = config["LOG_FILE"]
 
-# Логгер
+# Logger
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
 
-# Интенты
+# Intents
 intents = discord.Intents.all()
 
-# Бот
+# Bot
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 
-# Получение списка участников с ролью OLD_ROLE
+# Get list of members with OLD_ROLE
 async def get_members_with_old_role(guild):
     old_role = guild.get_role(OLD_ROLE_ID)
     return old_role.members if old_role else []
 
 
-# Смена роли и отправка сообщения в канал
+# Change role and send message in channel
 async def change_role_and_send_message(member, old_role, new_role, channel):
-    await member.remove_roles(old_role)
-    await member.add_roles(new_role)
-    await channel.send(f"{member.mention}, поздравляю с получением нового титула.")
+    try:
+        await member.remove_roles(old_role)
+        await member.add_roles(new_role)
+        await channel.send(f"{member.mention}, поздравляю с получением нового титула.")
+    except discord.Forbidden as e:
+        logging.error(f"Error changing role and sending message: {e}")
+        raise
 
 
-# Проверка наличия админской роли
+# Check for admin role
 def check_admin_role(ctx):
     admin_role = ctx.guild.get_role(ADMIN_ROLE_ID)
     return admin_role in ctx.author.roles if admin_role else False
 
 
-# Проверка времени пребывания участника на сервере
+# Check member's join time
 def check_join_time(member, threshold):
     join_time = member.joined_at.astimezone(pytz.timezone(TIMEZONE))
     current_time = datetime.datetime.now(pytz.timezone(TIMEZONE))
     return (current_time - join_time).total_seconds() > threshold
 
 
-# Команда check
+# Command check
 @bot.command()
 async def C(ctx, threshold: int = JOIN_TIME_THRESHOLD):
     if not check_admin_role(ctx):
@@ -73,34 +83,29 @@ async def C(ctx, threshold: int = JOIN_TIME_THRESHOLD):
     new_role = guild.get_role(NEW_ROLE_ID)
     channel = bot.get_channel(CHANNEL_ID)
 
-    if not old_role:
-        logging.error("Роль OLD_ROLE не найдена.")
-        await ctx.send("Роль OLD_ROLE не найдена.")
+    if not old_role or not new_role or not channel:
+        logging.error("Role or channel not found.")
+        await ctx.send("Role or channel not found.")
         return
 
-    if not new_role:
-        logging.error("Роль NEW_ROLE не найдена.")
-        await ctx.send("Роль NEW_ROLE не найдена.")
-        return
-
-    if not channel:
-        logging.error("Канал не найден.")
-        await ctx.send("Канал не найден.")
-        return
-
-    for member in await get_members_with_old_role(guild):
-        if check_join_time(member, threshold):
-            await change_role_and_send_message(member, old_role, new_role, channel)
+    members = await get_members_with_old_role(guild)
+    tasks = [
+        change_role_and_send_message(member, old_role, new_role, channel)
+        for member in members
+        if check_join_time(member, threshold)
+    ]
+    await asyncio.gather(*tasks)
 
 
-# Событие on_ready
+# Event on_ready
 @bot.event
 async def on_ready():
     logging.info(f"Logged in as {bot.user.name}")
 
 
-# Запуск бота
+# Run bot
 try:
     bot.run(TOKEN)
-except Exception as e:
-    logging.error(f"Произошла ошибка при запуске бота: {e}")
+except discord.LoginFailure as e:
+    logging.error(f"Failed to run bot: {e}")
+    raise
